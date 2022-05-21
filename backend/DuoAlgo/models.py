03d94d9d72d3a442e3abecbf54b2772b740a8574
Livia <livia.magureanu@gmail.com>
2022-05-21 17:29:57 +0300

    Debug #1

--- backend/DuoAlgo/models.py
+++ backend/DuoAlgo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
@@ -29,33 +30,33 @@ class Stage(models.Model):
 
 class Lesson(models.Model):
     # todo: change defaults
-    topic = models.ForeignKey("Topic", Topic, models.SET_NULL, blank=True, null=True)
-    stage = models.ForeignKey("Stage", Stage, models.SET_NULL, blank=True, null=True)
+    topic = models.ForeignKey("Topic", Topic, blank=True, null=True, default=None)
+    stage = models.ForeignKey("Stage", Stage, blank=True, null=True, default=None)
 
     title = models.CharField("Title", max_length=100)
-    author = models.ForeignKey("Credits to:", Author, models.SET_NULL, blank=True, null=True, default=None)
+    author = models.ForeignKey(Author, models.SET_NULL, blank=True, null=True, default=None, verbose_name="Credits to:")
 
     duration = models.DurationField("Lesson duration (optional)", blank=True, null=True, default=None)
     body = models.TextField("Lesson body in .md format", blank=True, null=True)
     link_to_code = models.URLField("Link to sample of code", blank=True, null=True, default=None)
 
-    tasks = models.ManyToManyField("List of related tasks", Task)
+    tasks = models.ManyToManyField(Task, verbose_name="List of related tasks")
 
 
 class Edge(models.Model):
-    src_node = models.ForeignKey(Lesson)
-    dest_node = models.ForeignKey(Lesson)
+    src_node = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name="src")
+    dest_node = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name="dest")
 
     # todo: add verification
 
 
-class UserExt(models.User):
-    class Meta:
-        proxy = True
+class UserExt(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, verbose_name="User")
 
     codeforces = models.CharField("Codeforces handle", max_length=50, blank=True, null=True, default=None)
     infoarena = models.CharField("Infoarena username", max_length=50, blank=True, null=True, default=None)
     varena = models.CharField("Varena username", max_length=50, blank=True, null=True, default=None)
 
-    completed_lessons = models.ManyToManyField("Already done", Lesson)
-    current_lesson = models.ForeignKey("Current lesson", Lesson, blank=True, null=True, default=None)
+    completed_lessons = models.ManyToManyField(Lesson, related_name="done", verbose_name="Already done")
+    current_lesson = models.ForeignKey(Lesson, blank=True, null=True, default=None, on_delete=models.SET_NULL,
+                                       related_name="current", verbose_name="Current lesson")
