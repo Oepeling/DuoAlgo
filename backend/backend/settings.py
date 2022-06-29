2269ebd5b64b30837023aff889c6e450946c60b1
Livia Magureanu <48083079+Oepeling@users.noreply.github.com>
2022-06-29 21:47:51 +0300

    static files (#6)
    * Deployment
    
    * static_root
    
    * static_root
    
    * static_root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
@@ -133,6 +134,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
