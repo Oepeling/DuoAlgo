3d0a55f09db52e4b9772696ac752c2d4955e2c2a
Livia <livia.magureanu@gmail.com>
2022-06-29 17:08:20 +0300

    backend: deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
